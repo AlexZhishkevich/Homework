7bdf1d5729bad41549cacdfda828a9a203ae4eb3
Alexey Zhishkevich <lexa2669847@gmail.com>
2021-01-02 14:06:59 +0300

    [!][Homework] Added MVC project with homework for 4 and 5 lessons

---
+++ HomeworkSolution/Lesson6/OnlineStore.WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OnlineStore.WebApplication.Models;
+
+namespace OnlineStore.WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private const string UsernameKey = "username";
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string username)
+        {
+            IActionResult result = null;
+            if (!string.IsNullOrEmpty(username))
+            {
+                HttpContext.Session.SetString(UsernameKey, username);
+                result = RedirectToAction(nameof(this.Index));
+            }
+            else
+            {
+                result = View();
+            }
+
+            return result;
+        }
+
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
